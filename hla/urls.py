584e0c513a03fe23188ef375b38b86c2be303632
chrispyatt <chris-pyatt@live.co.uk>
2020-09-22 15:59:19 +0100

    superfluous crap removed

--- hla/urls.py
+++ hla/urls.py
@@ -1,15 +1,11 @@
 from django.urls import path
 from hla import views
 from hla.models import ImportData
-from .views import HomePageView, SearchResultsView
+from .views import HomePageView
 
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
-    path("about/", views.about, name="about"),
-    path("contact/", views.contact, name="contact"),
-    path("hla/<name>", views.hello_there, name="hello_there"),
     path("import/", views.import_data, name="import"),
-    path("search_results/", SearchResultsView.as_view(), name="search_results"),
 ]
 
--- hla/views.py
+++ hla/views.py
@@ -19,37 +19,6 @@ class HomePageView(ListView):
         return object_list
 
 
-class SearchResultsView(ListView):
-    """Renders list of search results."""
-    model = Results
-    template_name = 'hla/search_results.html'
-
-    def get_queryset(self):
-        query = self.request.GET.get('q')
-        query_as_id = Patients.objects.get(patientNumber=query).patientID
-        object_list = Results.objects.filter(patientID=query_as_id)
-        return object_list
-
-
-def search_results(request):
-    return render(request, "hla/search_results.html")
-
-def about(request):
-    return render(request, "hla/about.html")
-
-def contact(request):
-    return render(request, "hla/contact.html")
-
-def hello_there(request, name):
-    return render(
-        request,
-        'hla/hello_there.html',
-        {
-            'name': name,
-            'date': datetime.now()
-        }
-    )
-
 def import_data(request):
     form = LogMessageForm(request.POST or None)
 
@@ -62,23 +31,3 @@ def import_data(request):
     else:
         return render(request, "hla/import_data.html", {"form": form})
 
-def addResult(request):
-    form = AddResult(request.POST or None)
-    if form.is_valid():
-        result = form.save(commit=False)
-        result.log_date = datetime.now()
-        result.save()
-        result_instance = Results.objects.create(testID='1', result='12:13', alleleID='2')
-        render(request, 'addResult.html.html')
-        return redirect("home")
-
-def addTest(request):
-    form = AddTest(request.POST or None)
-    if form.is_valid():
-        test = form.save(commit=False)
-        test.log_date = datetime.now()
-        test.save()
-        return redirect("home")
-
-def myView(request):
-    query_results = Results.object.all()
